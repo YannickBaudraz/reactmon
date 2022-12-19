cf5f86125a8a9d9c8112d70540fe0f99a16197b7
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-12-19 17:34:44 +0100

    Add example code with primereact

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, {useState} from 'react';
+import {Button} from 'primereact/button';
+
+import reactLogo from './assets/react.svg';
+import {Card} from 'primereact/card';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid">
+        <div className="col-10 col-offset-1">
+          <div className="surface-0">
+            <div>
+              <a href="https://vitejs.dev" target="_blank">
+                <img src="/vite.svg" className="logo" alt="Vite logo"/>
+              </a>
+              <a href="https://reactjs.org" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo"/>
+              </a>
+            </div>
+            <h1>Vite + PrimeReact</h1>
+            <div>
+              <h2>PrimeReact Typescript Issue Template</h2>
+              <p>
+                Please create a test case and attach the link to the to your github
+                issue report.
+              </p>
+            </div>
+            <Card title={'Counter'}>
+              <Button
+                  className="mr-2"
+                  onClick={() => setCount((count) => count + 1)}
+              >Count is {count}</Button>
+              <p>
+                Edit <code>src/App.tsx</code> and save to test PrimeReact
+              </p>
+            </Card>
+            <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+            </p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
