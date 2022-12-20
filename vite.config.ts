bd9c9385b7de6bb5d3993cb399227486bd2e2f48
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-12-20 12:24:31 +0100

    Configure for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react';
+import {VitePWA} from 'vite-plugin-pwa';
+import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return ({
+    plugins: [
+      VitePluginHtmlEnv({
+        prefix: '{{',
+        suffix: '}}',
+      }),
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: 'auto',
+        devOptions: {
+          enabled: true
+        },
+        includeAssets: ['favicon.ico', 'vite.svg'],
+        manifest: {
+          name: env.VITE_APP_NAME,
+          short_name: env.VITE_APP_NAME,
+          description: env.VITE_APP_DESCRIPTION,
+          theme_color: env.VITE_PWA_THEME_COLOR,
+          icons: [
+            {
+              src: 'vite.svg',
+              sizes: '192x192',
+              type: 'image/svg+xml',
+            },
+            {
+              src: 'vite.svg',
+              sizes: '512x512',
+              type: 'image/svg+xml',
+            }
+          ]
+        }
+      })
+    ]
+  });
+});
