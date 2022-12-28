35cecde07dff0eaa83fe783d50c13b099111bad9
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-12-28 10:26:05 +0100

    Add useless link to Pokémon item

--- src/components/PokemonItem/PokemonItem.tsx
+++ src/components/PokemonItem/PokemonItem.tsx
@@ -25,18 +25,20 @@ export default function PokemonItem({pokemon}: PokemonItemProps) {
   const imageSrc = `${import.meta.env.VITE_OFFICIAL_PNG_URL}/${pokemonId}.png`;
 
   return (
-      <Card
-          title={pokemon.name.capitalize()}
-          subTitle={pokemonId}
-          className="pokemon-item m-1 p-2 col max-w-fit"
-          onMouseOver={onMouseOver}
-          onMouseOut={onMouseOut}
-      >
-        <img
-            src={imageSrc}
-            alt={pokemon.name}
-            width="100"
-        />
-      </Card>
+      <a href="#" className="p-reset">
+        <Card
+            title={pokemon.name.capitalize()}
+            subTitle={pokemonId}
+            className="pokemon-item m-1 p-2 col max-w-fit"
+            onMouseOver={onMouseOver}
+            onMouseOut={onMouseOut}
+        >
+          <img
+              src={imageSrc}
+              alt={pokemon.name}
+              width="100"
+          />
+        </Card>
+      </a>
   );
 }
