3c91e5a782b0b0ed9b72d8e622b190601480596d
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-12-30 01:18:16 +0100

    Refactor navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,14 +8,22 @@ export interface NavBarProps {
 }
 
 export default function NavBar({onSearch}: NavBarProps) {
-  const start = <a href="/"><Image src="/pokedex-logo.png" alt="Logo" width="37"/></a>;
-
-  const end = <InputText
-      placeholder="Search"
-      type="search"
-      className="p-inputtext-sm"
-      onChange={onSearch}
-  />;
-
-  return <Menubar start={start} end={end} className="px-3"/>;
+  return <Menubar className="px-3" start={<NavBarStart/>} end={<NavBarEnd onSearch={onSearch}/>}/>;
 }
+
+function NavBarStart() {
+  return <a href="/"><Image src="/pokedex-logo.png" alt="Logo" width="37"/></a>;
+}
+
+function NavBarEnd({onSearch}: Partial<NavBarProps>) {
+  return <span className="p-input-icon-left">
+    <i className="pi pi-search"/>
+    <InputText
+        placeholder="Search"
+        type="search"
+        className="p-inputtext-sm"
+        onChange={onSearch}
+    />
+  </span>;
+}
+
